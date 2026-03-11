bea73d257daea9a159039767e0d035fd18b0f427
Nicolas-Menin <meninjuanmanuel542@gmail.com>
2026-03-11 16:52:43 -0300

    Arreglo de musica de fondo

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from window import GameWindow
 def main():
     """Se instancia la ventana y corre el juego"""
     window = GameWindow(WIDTH,HEIGHT,"arcade")
-    menu = window.game_over_view
+    menu = window.main_menu_view
+    window.music_bg.play()
     menu.setup()
     window.show_view(menu)
     arcade.run()
